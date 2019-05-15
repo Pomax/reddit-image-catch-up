b3f6e0ce77362cebc55089141c40b5c980650b9d
Pomax <pomax@nihongoresources.com>
2019-05-14 19:43:59 -0700

    This is the script you'll be running

--- catch-up.js
+++ catch-up.js
@@ -3,7 +3,7 @@
  * Run this with "node catch-up" and that's basically all you need to know.
  *
  * See the README.md on how to actually configure the script, or just look
- * in package.json and change it in a way that you can assume will work.
+ * in `package.json` and change it in a way that you can assume will work.
  *
  */
 
